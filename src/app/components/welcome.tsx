22799f9baf937103eebda850c37f3540ebef1751
Chayodom <xdtaka@gmail.com>
2025-05-27 11:17:07 +0700

    added conditonal rendering

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -1,7 +1,14 @@
 export default function Welcome() {
+    const text = "welcome to cosci"
+    // const currentYear = <p>2025</p>
+    const year = 2025
+    const isShow = false
     return (
         <div>
-            <p>Welcome to cosci</p>
+            <p>{text.toLocaleUpperCase()} {year}</p>
+            {/* {currentYear} */}
+            {isShow && <p>Date:10/10/1998</p>}
+            {isShow ? <p>Hello Next.js</p> : <p>Hello Javascript</p> }
         </div>
     );
 }
