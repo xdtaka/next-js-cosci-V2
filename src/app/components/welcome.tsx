1b6f1c5120c92ad598708db5ee14b02a32f37596
Chayodom <xdtaka@gmail.com>
2025-05-27 11:36:11 +0700

    added new tailwind

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -1,14 +1,20 @@
+'use client'
+
 export default function Welcome() {
     const text = "welcome to cosci"
     // const currentYear = <p>2025</p>
     const year = 2025
     const isShow = false
+    const handleClick = () => {
+        alert("Hello sir")
+    }
     return (
         <div>
-            <p>{text.toLocaleUpperCase()} {year}</p>
-            {/* {currentYear} */}
+            <p >{text.toLocaleUpperCase()} {year}</p>
             {isShow && <p>Date:10/10/1998</p>}
-            {isShow ? <p>Hello Next.js</p> : <p>Hello Javascript</p> }
+            <button className="bg-blue-700 text-amber-300 p-3 m-3 rounded-lg " onClick={handleClick}>Click Me</button>
+            {/* {isShow ? <p>Hello Next.js</p> : <p>Hello Javascript</p>} */}
+            
         </div>
     );
 }
