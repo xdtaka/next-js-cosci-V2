28a996d4f5521c552ba14fc2b1e84388620e961c
Chayodom <xdtaka@gmail.com>
2025-05-28 15:07:49 +0700

    addmore use of api

--- src/app/(front)/course/page.tsx
+++ src/app/(front)/course/page.tsx
@@ -1,9 +1,13 @@
 import Features05Page from "@/components/features-05/features-05";
 
-export default function Course() {
+export default async function Course() {
+
+    const data = await  fetch('https://api.codingthailand.com/api/course')
+    const courses = await data.json()
+
   return (
     <main>
-      <Features05Page/>
+          <Features05Page courses={courses} />
     </main>
   );
 }
--- src/components/features-05/features-05.tsx
+++ src/components/features-05/features-05.tsx
@@ -1,72 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import {
-  BookCheck,
-  ChartPie,
-  FolderSync,
-  Goal,
-  Users,
-  Zap,
-} from "lucide-react";
 
-const features = [
-  {
-    icon: Goal,
-    title: "Identify Opportunities",
-    description:
-      "Easily uncover untapped areas to explore and expand your reach effortlessly.",
-  },
-  {
-    icon: BookCheck,
-    title: "Build Authority",
-    description:
-      "Create valuable content that resonates, inspires trust, and positions you as an expert.",
-  },
-  {
-    icon: ChartPie,
-    title: "Instant Insights",
-    description:
-      "Gain immediate, actionable insights with a quick glance, enabling fast decision-making.",
-  },
-  {
-    icon: Users,
-    title: "Engage with Your Audience",
-    description:
-      "Boost audience engagement with interactive features like polls, quizzes, and forms.",
-  },
-  {
-    icon: FolderSync,
-    title: "Automate Your Workflow",
-    description:
-      "Streamline your processes by automating repetitive tasks, saving time and reducing effort.",
-  },
-  {
-    icon: Zap,
-    title: "Accelerate Growth",
-    description:
-      "Supercharge your growth by implementing strategies that drive results quickly and efficiently.",
-  },
-];
+type Features05PageProps = {
+  courses: any[]
+}
 
-const Features05Page = () => {
+
+const Features05Page = ({courses}:Features05PageProps) => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-screen-lg w-full py-10 px-6">
         <h2 className="text-4xl md:text-5xl md:leading-[3.5rem] font-bold tracking-tight sm:max-w-xl sm:text-center sm:mx-auto">
-          Boost Your Strategy with Smart Features
+          หลักสูตรที่กระผมมี
         </h2>
         <div className="mt-8 w-full mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
-          {features.map((feature) => (
+          {courses.map((course) => (
             <Card
-              key={feature.title}
+              key={course.id}
               className="flex flex-col border rounded-xl overflow-hidden shadow-none"
             >
               <CardHeader>
-                <feature.icon />
                 <h4 className="!mt-3 text-xl font-semibold tracking-tight">
-                  {feature.title}
+                  {course.title}
                 </h4>
                 <p className="mt-1 text-muted-foreground text-[17px]">
-                  {feature.description}
+                  {course.detail}
                 </p>
               </CardHeader>
               <CardContent className="mt-auto px-0 pb-0">
