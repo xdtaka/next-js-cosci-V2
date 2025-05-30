5e9fbac82e504e7b33c3bf5b63cafdb002831c6a
Chayodom <xdtaka@gmail.com>
2025-05-30 15:14:25 +0700

    dsad

--- src/components/navbar-01/navbar-01.tsx
+++ src/components/navbar-01/navbar-01.tsx
@@ -5,13 +5,6 @@ import { NavigationSheet } from "./navigation-sheet";
 import Link from "next/link";
 import { ShoppingBasket } from "lucide-react";
 import { Badge } from "../ui/badge";
-<<<<<<< HEAD
-import CountCartItem from "@/app/(front)/components/countCartItem";
-
-const Navbar01Page = () => {
-  return (
-    <div className="bg-amber-50">
-=======
 import CountCartItem from "@/app/(front)/components/CountCartItem";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
@@ -23,7 +16,6 @@ const Navbar01Page = async () => {
   });
   return (
     <div className="bg-muted">
->>>>>>> 7f91e2c (Created new)
       <nav className="h-16 bg-background border-b">
         <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
           <Logo />
@@ -32,25 +24,6 @@ const Navbar01Page = async () => {
           <NavMenu className="hidden md:block" />
 
           <div className="flex items-center gap-3">
-<<<<<<< HEAD
-
-            
-            <Link href="/cart">
-              <Badge className="p-2 text-sm flex items-center gap-2">
-                <ShoppingBasket />
-                <CountCartItem></CountCartItem>
-              </Badge>
-            </Link>
-
-
-            <Button variant="outline" className="hidden sm:inline-flex" asChild>
-              <Link href="/login">เข้าสู่ระบบ</Link>
-            </Button>
-            <Button asChild>
-              <Link href="/signup">สมัครสมาชิก</Link>
-            </Button>
-
-=======
             
             
             <Link href="/cart">
@@ -79,7 +52,6 @@ const Navbar01Page = async () => {
                 </>
               )
              }
->>>>>>> 7f91e2c (Created new)
             {/* Mobile Menu */}
             <div className="md:hidden">
               <NavigationSheet />
@@ -91,8 +63,4 @@ const Navbar01Page = async () => {
   );
 };
 
-<<<<<<< HEAD
 export default Navbar01Page;
-=======
-export default Navbar01Page;
->>>>>>> 7f91e2c (Created new)
